b9c00a42bdfe32fb956058d2579a767f01c687f6
Antonio Anes Fava Junior <afavajrbr80@gmail.com>
2021-08-12 21:06:30 -0400

    Version 1.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CepServiceService } from './cep-service.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CepAngular';
+
+  constructor(private cepsService: CepServiceService){}
+
+  consultaCep(valor:any, form:any){
+    this.cepsService.buscar(valor?.value).subscribe((dados) => this.populaForm(dados, form));
+ }
+
+ populaForm(dados:any, form:any){
+   form.setValue({
+     cep: dados.cep,
+     logradouro: dados.logradouro,
+     bairro: dados.bairro,
+     cidade: dados.localidade,
+     uf: dados.uf
+   })
+ }
 }
